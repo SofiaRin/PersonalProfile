699ac0afee6710fd185bdcfc39edb67e8f3f248f
SofiaRin <bujie02@sina.com>
2016-10-09 16:17:33 +0800

    修复跳页Bug,完善缓动动画触发

--- bin-debug/Main.js
+++ bin-debug/Main.js
@@ -152,11 +152,19 @@ var Main = (function (_super) {
         yin.x = 25;
         yin.y = 18;
         var icon = this.createBitmapByName("logo_png");
+        var iconrotat = 1;
         p1Container.addChild(icon);
         icon.scaleX = 0.5;
         icon.scaleY = 0.5;
-        icon.x = stageW / 2 - icon.width / 4;
-        icon.y = stageH / 8 + icon.height / 16;
+        icon.anchorOffsetX = 300;
+        icon.anchorOffsetY = 300;
+        icon.x = stageW / 2;
+        icon.y = stageH / 3.5;
+        /// 根据当前模式调整旋转度数或缩放正弦基数形成相应动画
+        icon.addEventListener(egret.Event.ENTER_FRAME, function (evt) {
+            /// 仅旋转
+            icon.rotation += iconrotat;
+        }, this);
         var title = new egret.TextField();
         p1Container.addChild(title);
         title.alpha = 0;
@@ -200,69 +208,116 @@ var Main = (function (_super) {
         p2Container.addChild(bg2);
         bg2.width = stageW;
         bg2.height = stageH;
+        var twpoem = egret.Tween.get(poem2);
         var poem2 = new egret.TextField();
         p2Container.addChild(poem2);
         poem2.alpha = 1;
         poem2.textColor = 0xffffff;
         poem2.textAlign = "center";
+        poem2.alpha = 1;
         poem2.text = "Forever";
         poem2.size = 24;
         poem2.strokeColor = 0xc0b0ca;
         poem2.stroke = 1;
         poem2.x = stageW - poem2.width - 10;
         poem2.y = stageH - poem2.height - 10;
-        var luopan = this.createBitmapByName("luopan_png");
+        var twpoem = egret.Tween.get(poem2);
+        if (currentpage == 2) {
+            twpoem.to({ "alpha": 0.1 }, 4000);
+        }
+        var luopan = this.createBitmapByName("luopan2_png");
         p2Container.addChild(luopan);
-        luopan.anchorOffsetX = 301;
-        luopan.anchorOffsetY = 325;
-        luopan.x = 0;
-        luopan.y = 0;
+        luopan.touchEnabled = true;
+        luopan.anchorOffsetX = luopan.width / 2;
+        luopan.anchorOffsetY = luopan.height / 2;
+        luopan.alpha = 0.3;
+        luopan.x = stageW / 2;
+        luopan.y = stageH / 2;
+        var twluopan = egret.Tween.get(luopan);
+        luopan.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            launchTween();
+        }, this);
+        function launchTween() {
+            /*** 本示例关键代码段开始 ***/
+            var funcChange = function () {
+                luopan.rotation += 6 * iDirection;
+            };
+            var iDirection = Math.random() > .5 ? -1 : 1; /// 随机方向
+            egret.Tween.get(luopan, { onChange: funcChange, onChangeObj: this })
+                .to({}, 300, egret.Ease.sineIn);
+            /*** 本示例关键代码段结束 ***/
+        }
         var xinggebg = new egret.Shape();
         xinggebg.graphics.beginFill(0x000000, 0.5);
-        xinggebg.graphics.drawRect(0, 0, stageW, 265);
+        xinggebg.graphics.drawRect(0, 0, stageW, 300);
         xinggebg.graphics.endFill();
         xinggebg.scaleX = 0.9;
         xinggebg.x = 30;
-        xinggebg.y = 325;
+        xinggebg.y = 396;
+        xinggebg.alpha = 0;
         p2Container.addChild(xinggebg);
         var textxingge = new egret.TextField();
         p2Container.addChild(textxingge);
-        textxingge.width = stageW - 80;
+        textxingge.width = stageW / 1.5;
         textxingge.x = 46;
-        textxingge.y = 360;
-        textxingge.height = 245;
+        textxingge.y = 81;
+        textxingge.alpha = 0;
+        textxingge.height = 300;
         textxingge.textColor = 0xffffff;
-        textxingge.size = 30;
+        textxingge.size = 28;
         textxingge.fontFamily = "Microsoft YaHei";
         textxingge.textAlign = "left";
         textxingge.text = "   关于性格，怎么说呢。拿两个关键字就是“外冷内热”和“多重性格”。取决于自己，有时候不善于和陌生人打交道。但在某种情况下的我可能并不会这样，反而会很善于去交流。比较随和，对于长时间没有联系过的圈子有种小抵触，自我感觉很难再融进去啊。";
-        var xingge = this.createBitmapByName("xingge_png");
-        p2Container.addChild(xingge);
-        xingge.x = stageW / 1.5;
-        xingge.y = stageH / 6;
         var tedianbg = new egret.Shape();
         tedianbg.graphics.beginFill(0x000000, 0.5);
-        tedianbg.graphics.drawRect(0, 0, stageW, 285);
+        tedianbg.graphics.drawRect(0, 0, stageW, 300);
         tedianbg.graphics.endFill();
         tedianbg.scaleX = 0.9;
         tedianbg.x = 30;
-        tedianbg.y = 815;
+        tedianbg.y = 396;
+        tedianbg.alpha = 0;
         p2Container.addChild(tedianbg);
         var texttedian = new egret.TextField();
         p2Container.addChild(texttedian);
-        texttedian.width = stageW - 88;
-        texttedian.x = 46;
-        texttedian.y = 825;
-        texttedian.height = 285;
+        texttedian.width = stageW / 1.5;
+        texttedian.x = stageW / 3.8;
+        texttedian.y = 805;
+        texttedian.alpha = 0;
+        texttedian.height = 300;
         texttedian.textColor = 0xffffff;
-        texttedian.size = 30;
+        texttedian.size = 28;
         texttedian.fontFamily = "Microsoft YaHei";
         texttedian.textAlign = "left";
-        texttedian.text = "    每次一说有什么特长之类的,就感觉比较尴尬。自认为我就是那种九年义务教务流水线式组装出来的学生。也是一直让我觉得比较后悔，没在之前的日子里对自己未来的路多做一些探索。\n    探索是必要的，毕竟不做不知理。有时候我也问自己到底有什么特点，但试着做一些东西出来之后，回过头总结才看到这就是我。";
+        texttedian.text = "    自认为我就是那种九年义务教务流水线式组装出来的学生。也是一直让我觉得比较后悔，没在之前的日子里对自己未来的路多做一些探索。\n    探索是必要的，毕竟不做不知理。有时候我也问自己到底有什么特点，但试着做一些东西出来之后，回过头总结才看到这就是我。";
         var tedian = this.createBitmapByName("tedian_png");
         p2Container.addChild(tedian);
-        tedian.x = 0;
-        tedian.y = stageH / 1.75;
+        tedian.scaleX = 1.2;
+        tedian.scaleY = 1.2;
+        tedian.x = stageW / 2 - tedian.width;
+        tedian.y = stageH / 2;
+        var xingge = this.createBitmapByName("xingge_png");
+        p2Container.addChild(xingge);
+        xingge.scaleX = 1.2;
+        xingge.scaleY = 1.2;
+        xingge.x = stageW / 2 - 30;
+        xingge.y = stageH / 2 - tedian.height * 1.2;
+        p2Container.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            p2launchTween();
+        }, this);
+        function p2launchTween() {
+            egret.Tween.get(tedian, {})
+                .to({ x: 35 }, 600, egret.Ease.sineIn).to({ y: stageH / 1.37 }, 400);
+            egret.Tween.get(xingge, {})
+                .to({ x: 470 }, 600, egret.Ease.sineIn).to({ y: stageH / 17 }, 400);
+            egret.Tween.get(texttedian, {})
+                .to({ "alpha": 1 }, 600, egret.Ease.sineIn);
+            egret.Tween.get(textxingge, {})
+                .to({ "alpha": 1 }, 600, egret.Ease.sineIn);
+            egret.Tween.get(tedianbg, {}).to({ "alpha": 1 }, 400)
+                .to({ y: 785 }, 500, egret.Ease.sineIn);
+            egret.Tween.get(xinggebg, {}).to({ "alpha": 1 }, 400)
+                .to({ y: 51 }, 500, egret.Ease.sineIn);
+        }
         //page3 - start
         var bg3 = this.createBitmapByName("mbg33_jpg");
         p3Container.addChild(bg3);
@@ -273,14 +328,16 @@ var Main = (function (_super) {
         zijibg.graphics.drawRect(0, 0, stageW, 345);
         zijibg.graphics.endFill();
         zijibg.scaleX = 0.9;
+        zijibg.alpha = 0;
         zijibg.x = 30;
-        zijibg.y = 150;
+        zijibg.y = 50;
         p3Container.addChild(zijibg);
         var textziji = new egret.TextField();
         p3Container.addChild(textziji);
         textziji.width = stageW - 90;
         textziji.x = 46;
-        textziji.y = 190;
+        textziji.alpha = 0;
+        textziji.y = 630;
         textziji.height = 345;
         textziji.textColor = 0xffffff;
         textziji.size = 30;
@@ -290,15 +347,28 @@ var Main = (function (_super) {
         var ziji = this.createBitmapByName("ziji_png");
         p3Container.addChild(ziji);
         ziji.x = stageW / 2 - ziji.width / 2;
-        ziji.y = 40;
+        ziji.y = 800;
         var choice = this.createBitmapByName("choice_png");
         p3Container.addChild(choice);
+        choice.touchEnabled = true;
         choice.anchorOffsetX = 218;
         choice.anchorOffsetY = 209;
         choice.scaleX = 1.55;
         choice.scaleY = 1.55;
         choice.x = stageW / 2;
-        choice.y = stageH - choice.height / 2;
+        choice.y = stageH / 1.3;
+        p3Container.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            p3launchTween();
+        }, this);
+        function p3launchTween() {
+            /*** 本示例关键代码段开始 ***/
+            egret.Tween.get(ziji, {})
+                .to({ y: 40 }, 1000, egret.Ease.sineIn);
+            egret.Tween.get(textziji, {})
+                .to({ y: 190 }, 1200, egret.Ease.sineIn).to({ "alpha": 1 }, 600);
+            egret.Tween.get(zijibg, {}).to({ y: 150 }, 600)
+                .to({ "alpha": 1 }, 800, egret.Ease.sineIn);
+        }
         var poem3 = new egret.TextField();
         p3Container.addChild(poem3);
         poem3.alpha = 1;
@@ -310,19 +380,6 @@ var Main = (function (_super) {
         poem3.stroke = 1;
         poem3.x = stageW - poem3.width - 10;
         poem3.y = stageH - poem3.height - 10;
-        /*
-               bgContainer.addChild(p1Container);
-               p1Container.x = 0;
-               p1Container.y = 0;
-        
-               bgContainer.addChild(p2Container);
-               p2Container.x = 0;
-               p2Container.y = stageH;
-        
-               bgContainer.addChild(p3Container);
-               p3Container.x = 0;
-               p3Container.y = 2*stageH;
-        */
         //页面滑动功能
         this.scrollRect = new egret.Rectangle(0, 0, this.stage.stageWidth, 3 * stageH);
         this.cacheAsBitmap = true;
@@ -330,15 +387,18 @@ var Main = (function (_super) {
         var init_TouchPointY = 0; //起始触摸点
         var init_StagePointY = 0; //起始Stage点
         var MoveDistance = 0; //移动距离
+        var mark = 0;
+        var currentpage = 1;
         this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, startScroll, this); //监听事件1
         function startScroll(e) {
             if ((this.scrollRect.y % stageH) != 0) {
                 this.scrollRect.y = init_StagePointY; //每次滑动都卡主一个stage的高度
             }
+            mark = e.stageY;
             init_TouchPointY = e.stageY;
             init_StagePointY = this.scrollRect.y;
-            this.addEventListener(egret.TouchEvent.TOUCH_MOVE, onScroll, this); //实时计算滑动的距离值
         }
+        this.addEventListener(egret.TouchEvent.TOUCH_MOVE, onScroll, this); //实时计算滑动的距离值 监听事件2
         function onScroll(e) {
             var rect = this.scrollRect;
             MoveDistance = init_TouchPointY - e.stageY;
@@ -347,16 +407,19 @@ var Main = (function (_super) {
                 this.scrollRect = rect;
             }
         }
-        this.addEventListener(egret.TouchEvent.TOUCH_END, stopScroll, this); //监听事件2
+        this.addEventListener(egret.TouchEvent.TOUCH_END, stopScroll, this); //监听事件3
         function stopScroll(e) {
             var rect = this.scrollRect;
-            if ((MoveDistance >= (this.stage.stageHeight / 3)) && init_StagePointY != 2 * stageH) {
+            var origin = mark - e.stageY;
+            if ((MoveDistance >= (stageH / 3)) && init_StagePointY != 2 * stageH && origin != 0) {
                 rect.y = init_StagePointY + stageH;
                 this.scrollRect = rect;
+                currentpage++;
             }
-            else if ((MoveDistance <= (-(this.stage.stageHeight / 3))) && init_StagePointY != 0 && MoveDistance != 0) {
+            else if ((MoveDistance <= (-stageH / 3)) && init_StagePointY != 0 && origin != 0) {
                 rect.y = init_StagePointY - stageH;
                 this.scrollRect = rect;
+                currentpage--;
             }
             else {
                 rect.y = init_StagePointY;
@@ -364,7 +427,6 @@ var Main = (function (_super) {
             }
             this.stage.removeEventListener(egret.TouchEvent.TOUCH_MOVE, onScroll, this);
         }
-        //页面滑动功能结束
         //根据name关键字，异步获取一个json配置文件，name属性请参考resources/resource.json配置文件的内容。
         // Get asynchronously a json configuration file according to name keyword. As for the property of name please refer to the configuration file of resources/resource.json.
         RES.getResAsync("description_json", this.startAnimation, this);
